f806536e5e2236180b590c64ff0c667cc1e504fa
atifshehzad668 <151611053+atifshehzad668@users.noreply.github.com>
2024-01-03 10:12:11 +0500

    circle dist listing

--- app/Http/Controllers/DistrictController.php
+++ app/Http/Controllers/DistrictController.php
@@ -3,8 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Districts;
 
 class DistrictController extends Controller
 {
-    //
+    public function listdistricts()
+    {
+        $data = [
+            'title' => 'List Districts',
+        ];
+
+        $query = Districts::query();
+
+
+        $isSuperAdmin = auth()->user()->roles->pluck('name')[0] == "Super Admin";
+
+
+        if (!$isSuperAdmin) {
+            return [];
+        }
+
+        $data["data"] = $query->get();
+
+        return view("district.list", $data);
+    }
 }
